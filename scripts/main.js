cd32518dc4c1731792a7f5ae57dc88e81ff9dfe4
n.zinoviev <zinoviev.sd@gmail.com>
2025-01-08 22:05:45 +0300

    Return image change on click

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ myImage.addEventListener("click", () => {
    }
 });
 
+/*
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
@@ -32,3 +33,4 @@ if (!localStorage.getItem("name")) {
 myButton.addEventListener("click", () => {
    setUserName();
 });
+*/
